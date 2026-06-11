a72061849a247ee9cf70209e32106c7add41d282
B. Allanigue <ballanigue.dev@gmail.com>
2026-06-12 01:14:53 +0800

    Update password-generation.py

--- password-generation.py
+++ password-generation.py
@@ -1,1 +1,11 @@
-# will make this one soon
+import random
+
+q = """
+1. Generate Password
+2. Exit
+"""
+
+while True:
+    choice = input("Select your option: ")
+    if choice == "1":
+        print("Which one do you want?") 
